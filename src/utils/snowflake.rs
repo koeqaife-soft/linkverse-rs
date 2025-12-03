73f5def2cf1749bedf8344805e628e696ef49bfc
Danik <retikadni@gmail.com>
2025-12-03 01:04:21 +0100

    Make todo

--- src/utils/snowflake.rs
+++ src/utils/snowflake.rs
@@ -7,10 +7,6 @@ const COUNTER_BITS: u64 = 12;
 const PID_BITS: u64 = 5;
 const SID_BITS: u64 = 5;
 
-task_local! {
-    static THREAD_ID: u8;
-}
-
 #[derive(Debug)]
 pub struct SnowflakeGenerator {
     last_timestamp: AtomicU64,
@@ -33,34 +29,34 @@ impl SnowflakeGenerator {
     }
 
     pub async fn generate(&self) -> u64 {
-        THREAD_ID.with(|tid| {
-            let tid = *tid as u64;
-            let mut ts = Self::current_time_ms() - EPOCH;
+        todo!("Separate generator between worker threads");
 
-            let mut counter = self.counter.load(Ordering::Relaxed);
+        let tid = 0;
+        let mut ts = Self::current_time_ms() - EPOCH;
 
-            loop {
-                let last_ts = self.last_timestamp.load(Ordering::Relaxed);
-                if ts == last_ts {
-                    counter = self.counter.fetch_add(1, Ordering::Relaxed);
-                    if counter >= (1 << COUNTER_BITS) {
-                        ts = Self::current_time_ms() - EPOCH;
-                        self.counter.store(0, Ordering::Relaxed);
-                        continue;
-                    }
-                } else {
-                    counter = 0;
+        let mut counter: u64;
+
+        loop {
+            let last_ts = self.last_timestamp.load(Ordering::Relaxed);
+            if ts == last_ts {
+                counter = self.counter.fetch_add(1, Ordering::Relaxed);
+                if counter >= (1 << COUNTER_BITS) {
+                    ts = Self::current_time_ms() - EPOCH;
                     self.counter.store(0, Ordering::Relaxed);
+                    continue;
                 }
-                self.last_timestamp.store(ts, Ordering::Relaxed);
-                break;
+            } else {
+                counter = 0;
+                self.counter.store(0, Ordering::Relaxed);
             }
+            self.last_timestamp.store(ts, Ordering::Relaxed);
+            break;
+        }
 
-            (ts << (COUNTER_BITS + SID_BITS + PID_BITS))
-                | ((tid & ((1 << PID_BITS) - 1)) << (COUNTER_BITS + SID_BITS))
-                | ((self.server_id as u64 & ((1 << SID_BITS) - 1)) << COUNTER_BITS)
-                | counter
-        })
+        (ts << (COUNTER_BITS + SID_BITS + PID_BITS))
+            | ((tid & ((1 << PID_BITS) - 1)) << (COUNTER_BITS + SID_BITS))
+            | ((self.server_id as u64 & ((1 << SID_BITS) - 1)) << COUNTER_BITS)
+            | counter
     }
 
     pub fn parse(id: u64) -> (f64, u8, u8, u16) {
