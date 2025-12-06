abf3beb9cb0ecba5bb1e1b0cc3e295953606a2d7
Danik <retikadni@gmail.com>
2025-12-06 12:47:58 +0100

    Login endpoint (wip)

--- src/endpoints/auth.rs
+++ src/endpoints/auth.rs
@@ -1,6 +1,60 @@
-use axum::Router;
+use axum::{Router, extract::State, http::StatusCode, routing::post};
+use serde::Deserialize;
+use validator::Validate;
 
-use crate::utils::state::ArcAppState;
+use crate::{
+    database::{
+        auth::{Tokens, create_tokens, get_user_by_email},
+        lazy::LazyConn,
+    },
+    utils::{
+        response::{ApiResponse, AppError, log_and_convert, ok},
+        security::check_password,
+        state::ArcAppState,
+        validate::ValidatedJson,
+    },
+};
+
+#[derive(Deserialize, Validate)]
+struct LoginPayload {
+    #[validate(length(min = 8))]
+    password: String,
+
+    #[validate(email)]
+    email: String,
+}
+
+async fn login(
+    ValidatedJson(payload): ValidatedJson<LoginPayload>,
+    State(state): State<ArcAppState>,
+) -> Result<ApiResponse<Tokens>, AppError> {
+    let conn_unlocked = LazyConn::new(state.db_pool.clone());
+    let mut conn = conn_unlocked.lock().await;
+
+    // Getting user
+    let user_result = get_user_by_email(payload.email, &mut conn)
+        .await
+        .map_err(log_and_convert)?;
+    if user_result.is_none() {
+        return Err(AppError::NotFound("USER_NOT_FOUND".to_string()));
+    }
+
+    // Checking password
+    let user = user_result.unwrap();
+    let correct = check_password(&user.password_hash, &payload.password);
+    if !correct {
+        return Err(AppError::Unauthorized("INCORRECT_PASSWORD".to_string()));
+    }
+
+    // Generating tokens
+    let mut tx = conn.transaction().await.map_err(log_and_convert)?;
+    let tokens = create_tokens(user.user_id, &mut tx, state)
+        .await
+        .map_err(log_and_convert)?;
+    tx.commit().await.map_err(log_and_convert)?;
+
+    return Ok(ok(tokens, StatusCode::OK));
+}
 
 pub fn router() -> Router<ArcAppState> {
     Router::new()
